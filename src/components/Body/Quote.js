56f7a6e971f934b1f7c30efa256a0dc2d3102df2
Guillaume <guillaume@debeire.com>
2021-09-01 17:18:56 +0200

    css better

--- src/components/Body/Quote.js
+++ src/components/Body/Quote.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Quote = ({ name, year, text }) => {
     return (
     <article className="quote">
-        <p className="quote-text">"{text}"</p>
+        <span className="quote-symbol">&#10077;</span>
+        <p className="quote-text">{text}"</p>
         <p className="quote-name">{name}</p>
     </article>
 );
