8cd2ff13564ef890e4a3fdb0353ee18088eeefd2
Guillaume <guillaume@debeire.com>
2021-09-01 17:38:01 +0200

    mmh

--- src/components/Body/Quote.js
+++ src/components/Body/Quote.js
@@ -5,6 +5,7 @@ const Quote = ({ name, year, text }) => {
     <article className="quote">
         <span className="quote-symbol">&#10077;</span>
         <p className="quote-text">{text}"</p>
+        <span className="quote-symbol2">&#10078;</span>	
         <p className="quote-name">{name}</p>
     </article>
 );
